b294aab3197a3e23ece3bb065544abd788642ebf
MalshaniDahanyaka <mekaladahanayaka80@gmail.com>
2023-02-05 11:00:53 +0530

    Add SignupRequest model for user signup endpoint #feature/signup

--- src/main/java/com/isa/teachingInstitution/Model/Request/SignupRequest.java
+++ src/main/java/com/isa/teachingInstitution/Model/Request/SignupRequest.java
@@ -3,17 +3,17 @@ package com.isa.teachingInstitution.Model.Request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
 @AllArgsConstructor
-public class StudentSignup {
+@Data
+public class SignupRequest {
 
+    private String firstName;
+    private String lastName;
     private String username;
     private String email;
     private String password;
-    private String firstName;
-    private String lastName;
-    private String studentID;
-    private String department;
-    private String address;
+    private String role;
+    private String userID;
+
 
 }
