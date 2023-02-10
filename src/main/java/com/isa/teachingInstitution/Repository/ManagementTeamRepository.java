012f0ac583adcf1f7200d7ef9cec32247fae4fa2
MalshaniDahanyaka <mekaladahanayaka80@gmail.com>
2023-02-10 16:20:01 +0530

    Add management team repository class

---
+++ src/main/java/com/isa/teachingInstitution/Repository/ManagementTeamRepository.java
@@ -0,0 +1,7 @@
+package com.isa.teachingInstitution.Repository;
+
+import com.isa.teachingInstitution.Model.ManagementTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManagementTeamRepository extends JpaRepository<ManagementTeam, String> {
+}
